38eb64d83bba1d7d99e0769272860b7c485aea5e
Amritha_Anil <amrithapanil0326@gmail.com>
2026-04-05 19:51:28 +0200

    fix: clean build

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,6 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
   const location = useLocation();
 
   useEffect(() => {
